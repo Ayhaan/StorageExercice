37bc82f6decc0ef74f0c80c2c45c40da1b83c868
Ayhan <caliskanayhan@hotmail.fr>
2021-04-28 12:07:51 +0200

    upload file

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    public function create(){
+        return view('admin.createFile');
+    }
+
+    public function store(Request $request){
+        // dd($request->file('img'));
+        
+        //Storage
+        Storage::put('public/img/', $request->file('img'));
+
+        //stockage
+        $file = new File();
+        $file->img = $request->file('img')->hashName();
+        $file->save();
+        return redirect()->route('home');
+    }
+
+}
